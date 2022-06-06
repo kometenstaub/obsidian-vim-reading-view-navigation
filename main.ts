f9ced10f75f614ddc381f341652d9e912808bf8b
kometenstaub <83140328+kometenstaub@users.noreply.github.com>
2022-06-06 15:18:20 +0200

    feat: emit event for vim yank

--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
-import { Editor, MarkdownView, Notice, Plugin, htmlToMarkdown } from "obsidian";
+import { Editor, EditorPosition, EventRef, Events, MarkdownView, Notice, Plugin, htmlToMarkdown } from "obsidian";
 import clipboardModifications from "clipboardModification";
+import { around } from "monkey-around";
 
 interface PasteFunction {
 	(this: HTMLElement, ev: ClipboardEvent): void;
@@ -13,16 +14,52 @@ declare module "obsidian" {
 	}
 }
 
+class YankEvent extends Events {
+	on(name: "vim-yank", callback: (text: string) => [EditorPosition, EditorPosition, string]): EventRef;
+	on(name: string, callback: (...data: any) => any, ctx?: any): EventRef {
+		return super.on(name, callback, ctx);
+	}
+}
+
+
 export default class SmarterPasting extends Plugin {
 	pasteFunction: PasteFunction;
+	yankEventUninstaller: any;
+	uninstall = false;
 
 	async onload() {
+		if (this.app.vault.getConfig("vimMode")) {
+			this.uninstall = true;
+			const yank = new YankEvent();
+			// @ts-ignore
+			this.yankEventUninstaller = around(window.CodeMirrorAdapter?.Vim.getRegisterController(), {
+				pushText(oldMethod: any) {
+					return function(...args: any[]) {
+						if (args.at(1) === "yank")
+							yank.trigger("vim-yank", args.at(2));
+
+						const result = oldMethod && oldMethod.apply(this, args);
+						return result;
+					};
+				}
+			});
+			this.registerEvent(
+				yank.on("vim-yank", (text) => {
+					const cursorFrom = this.app.workspace.getActiveViewOfType(MarkdownView).editor.getCursor("from");
+					const cursorTo = this.app.workspace.getActiveViewOfType(MarkdownView).editor.getCursor("to");
+					// console.log(cursorFrom, cursorTo, text);
+					return [cursorFrom, cursorTo, text];
+				}
+				)
+			);
+		}
 		console.log("Pasta Copinara Plugin loaded.");
 
 		this.pasteFunction = this.modifyPasteEvent.bind(this); // Listen to paste event
 
 		this.registerEvent(
 			this.app.workspace.on("editor-paste", this.pasteFunction)
+
 		);
 
 		this.addCommand({
@@ -33,7 +70,12 @@ export default class SmarterPasting extends Plugin {
 
 
 	}
-	async onunload() { console.log("Pasta Copinara Plugin unloaded.") }
+	async onunload() {
+		if (this.uninstall) 
+			this.yankEventUninstaller();
+		
+		console.log("Pasta Copinara Plugin unloaded.");
+	}
 
 	private getEditor(): Editor {
 		const activeLeaf = this.app.workspace.getActiveViewOfType(MarkdownView);
