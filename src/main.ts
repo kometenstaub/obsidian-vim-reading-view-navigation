13b87fb89876cdfc6749cc13e3b45ba668d4d0c7
kometenstaub <83140328+kometenstaub@users.noreply.github.com>
2022-08-19 13:33:39 +0200

    chore: run prettier

--- src/main.ts
+++ src/main.ts
@@ -53,7 +53,7 @@ const registerScopes = (scope: Scope, plugin: VimReadingViewNavigation) => {
 			if (self.keyArray.length === 0) {
 				addEventListener('keydown', self.jumpTopEvent);
 				self.keyArray.push(evt.key);
-                return false;
+				return false;
 			}
 		}
 		return true;
